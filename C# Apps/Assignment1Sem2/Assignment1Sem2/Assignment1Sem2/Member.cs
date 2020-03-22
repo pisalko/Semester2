ceddd9b99f3843e64ab9e370c3f2672f7e8b9b68
Valentin Vasilev <i432543@fhict.nl>
2020-03-22 05:41:23 +0100

    some work on c# assignment

---
+++ C# Apps/Assignment1Sem2/Assignment1Sem2/Assignment1Sem2/Member.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Sem2
+{
+    public class Member
+    {
+        private string namee = "";
+        private int agee = 0;
+        private string packagee = "";
+        public Member(string name, int age, string package)
+        {
+            namee = name;
+            agee = age;
+            packagee = package;
+        }
+        public string getName()
+        {
+            return namee;
+        }
+        public int getAge()
+        {
+            return agee;
+        }
+        public string getPackageAccess()
+        {
+            return packagee;
+        }
+    }
+}
